234271fb6261a8820d39db8ab3b20a7a94ef5df6
ted111712 <ted111712@gmail.com>
2026-08-15 16:28:13 +0900

    feat(fetch): API 엔드포인트 등록 해제

--- src/main.py
+++ src/main.py
@@ -1,11 +1,3 @@
 from fastapi import FastAPI
 
-from src.api.v1.ratings import router as ratings_router
-
 app = FastAPI()
-
-app.include_router(
-    ratings_router,
-    prefix="/api/v1/video-ratings",
-    tags=["video-ratings"],
-)
